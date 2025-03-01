3c0a970220636a11550c9bb4b1bafcdd4fe25ea4
Drishti-byte <drishtibansal06@gmail.com>
2025-03-01 10:09:27 +0530

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,5 @@
+newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+newBtn.style.backgroundColor = "red";
+newBtn.style.color = "white"; 
+document.querySelector("body").prepend(newBtn);
